54a6e5e9e4c8a5a53e0d01df2993ff99f5378fb7
BRMarta <martax87@hotmail.com>
2021-06-04 17:55:58 +0200

    Update 04/06

---
+++ FotosXML/0406-fotos.js
@@ -0,0 +1,51 @@
+
+let usuarios = [];
+
+function leerXML0() {
+    // lee desde GitHub.
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            miFuncion(this, "XML propio");
+        }
+    };
+    xhr.open("GET", "https://brmarta.github.io/CMV-publico/XML/datos.xml", true);
+    xhr.send();
+}
+
+function miFuncion(xml, fuente,cantidad) {
+    console.log(cantidad);
+    let listaAviones;
+    let i;
+    let nombre;
+    let foto;
+    let aviones = [];
+    let bloque="";
+    let xmlDoc = xml.responseXML;
+
+    let x = xmlDoc.getElementsByTagName("avion");
+
+    for (i = 0; i < x.length; i++) {
+        nombre = x[i].getElementsByTagName("nombre")[0].childNodes[0].nodeValue;
+        foto = x[i].getElementsByTagName("foto")[0].childNodes[0].nodeValue;
+        pie = x[i].getElementsByTagName("pie")[0].childNodes[0].nodeValue;
+        //detalle = x[i].getElementsByTagName("detalle")[0].childNodes[0].nodeValue;
+        //avion = [nombre, foto, pie, detalle];
+        avion = [nombre, foto, pie];
+        aviones.push(avion);
+    }
+
+        for (i = 0; i < aviones.length; i++) {
+        
+            let titulo = aviones[i][0];
+            let imagen = aviones[i][1];
+            let pie = aviones[i][2];
+
+            
+            //bloque += "<figure><img src='"+imagen+"'/></figure>";
+           
+            bloque += "<figure><h3>"+titulo+"</h3><img src='"+imagen+"'/><figcaption><p>"+pie+"</p></figcaption></figure>";
+           //class=\"divPruebaSegundo\"> 
+        }
+    document.getElementById("fotos").innerHTML = bloque;
+}
